d99c77d3297a822235ff3c9f4eefcc71151a3559
Ramanujam Gond <ramanujamgond@gmail.com>
2024-01-05 21:00:42 +0530

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+
+  return (
+    <>
+      <div className="main_wrapper">
+        <div className="inner_wrapper">
+          <div className="to_do_heading">Get things Done</div>
+          <div className="add_task_wrapper">
+            <input />
+            <button>Add Task</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
