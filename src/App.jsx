16c4eca8ea0b77cd8ebedad666e794c03427bc8e
Ramanujam Gond <ramanujamgond@gmail.com>
2024-01-08 20:13:56 +0530

    feat: develop the UI of the to do list app

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,43 @@ function App() {
     <>
       <div className="main_wrapper">
         <div className="inner_wrapper">
-          <div className="to_do_heading">Get things Done</div>
+          <div className="to_do_heading">Get things Done!</div>
           <div className="add_task_wrapper">
-            <input />
+            <input type="text" placeholder="What is the task today?" />
             <button>Add Task</button>
+          </div>
+          <div className="task_list_wrapper">
+            <div className="task_cards">
+              <span className="text-label">eat the milk</span>
+              <span className="icon_wrapper">
+                <span><i className="bi bi-pencil-square"></i></span>
+                <span><i className="bi bi-trash ms-3"></i></span>
+              </span>
+            </div>
+
+            <div className="task_cards">
+              <span className="text-label">eat the milk</span>
+              <span className="icon_wrapper">
+                <span><i className="bi bi-pencil-square"></i></span>
+                <span><i className="bi bi-trash ms-3"></i></span>
+              </span>
+            </div>
+
+            <div className="task_cards">
+              <span className="text-label">eat the milk</span>
+              <span className="icon_wrapper">
+                <span><i className="bi bi-pencil-square"></i></span>
+                <span><i className="bi bi-trash ms-3"></i></span>
+              </span>
+            </div>
+
+            <div className="task_cards">
+              <span className="text-label">eat the milk</span>
+              <span className="icon_wrapper">
+                <span><i className="bi bi-pencil-square"></i></span>
+                <span><i className="bi bi-trash ms-3"></i></span>
+              </span>
+            </div>
           </div>
         </div>
       </div>
